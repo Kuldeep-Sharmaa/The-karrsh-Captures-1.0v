c4699032429c689dbd683dd052e2cebf3601b639
Kuldeep-Sharmaa <153490879+Kuldeep-Sharmaa@users.noreply.github.com>
2025-02-07 18:05:35 +0530

    update logic

--- script.js
+++ script.js
@@ -269,21 +269,3 @@ startAutoScroll();
 
 carousel.addEventListener("mouseenter", stopAutoScroll);
 carousel.addEventListener("mouseleave", startAutoScroll);
-
-// auto scroling text
-document.addEventListener("DOMContentLoaded", function () {
-  const words = document.querySelectorAll(".rolling-text .word");
-  let index = 0;
-
-  setInterval(() => {
-    words.forEach((word) => (word.style.opacity = 0));
-    words[index].style.opacity = 1;
-    words[index].style.transform = "translateY(0)";
-
-    setTimeout(() => {
-      words[index].style.transform = "translateY(-100%)";
-    }, 3000);
-
-    index = (index + 1) % words.length;
-  }, 4000);
-});
